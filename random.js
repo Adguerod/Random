d3ca56fcacb495766bdff1714fe2031df7ef0e51
Adrian <adguerod@gmail.com>
2015-08-10 20:41:07 +0100

    random version 2

--- random.js
+++ random.js
@@ -1,9 +1,9 @@
 var numero = Math.random();
 
-var str=' MAYOR que 0,5';
+var str=' MAYOOOOOOOOOOR que 0,5';
 
 if (numero <= 0.5){
-str = ' MENOR que 0,5';
+str = ' MENOOOOOOOOOOR que 0,5';
 }
 
 console.log ('\n' + numero + str +'\n');
